b8541a8a561e0774bd276ac66bd9d5a7c951f942
Cleiston <cleistonrs@gmail.com>
2020-07-13 16:07:31 -0300

    finish settings screen

--- src/screens/Settings/index.js
+++ src/screens/Settings/index.js
@@ -1,11 +1,94 @@
 import React from 'react';
+import { Ionicons } from '@expo/vector-icons';
 
-import { Container } from './styles';
+import { 
+    Wrapper, 
+    Container,
+    Header,
+    HeaderHelp,
+    HeaderUserInfo,
+    HeaderMyAccount,
+    Username,
+    Fullname,
+    ViewProfile,
+    ViewProfileText,
+    Img,
+    Section,
+    Item,
+    ClickableItem,
+    ItemTitle,
+    ItemDescription
+} from './styles';
+
+import userImg from '../../images/user.png';
+
+const items = [
+    {
+        title: 'Meu PicPay',
+        description: '@cleiston'
+    },
+    {
+        title: 'Meu número',
+        description: '(**)****-9332'
+    },
+    {
+        title: 'Meu E-mail',
+        description: 'c*******s@g****.com'
+    },
+    {
+        title: 'Dados pessoais',
+        description: 'Nome, CPF e data de nascimento'
+    },
+    {
+        title: 'Conta Bancária',
+        description: 'Taxas e Limites'
+    },
+    {
+        title: 'Meus endereços',
+    },
+    {
+        title: 'Meus favoritos',
+    },
+    {
+        title: 'Validar identidade',
+        description: 'Não verificado'
+    }
+];
 
 export default function Settings() {
     return (
-        <Container>
-            
-        </Container>
+        <Wrapper>
+            <Container>
+                <Header>
+                    <HeaderHelp>Ajuda</HeaderHelp>
+                    <HeaderUserInfo>
+                        <Img source={userImg} />
+                        <Username>@cleiston</Username>
+                        <Fullname>Cleiston Rodrigues</Fullname>
+                        <ViewProfile>
+                            <ViewProfileText>Ver meu perfil</ViewProfileText> 
+                            <Ionicons name="ios-arrow-forward" size={24} color="#00ab4b" style={{padding: 5}} />
+                        </ViewProfile>
+                    </HeaderUserInfo>
+                    <HeaderMyAccount>
+                        Minha Conta
+                    </HeaderMyAccount>
+                </Header>
+                <Section>
+                    {items.map((item, index) => (
+                        <Item key={String(index)}>
+                            <ClickableItem>
+                                <ItemTitle>
+                                    {item.title}
+                                </ItemTitle>
+                                <ItemDescription>
+                                    {item.description}
+                                </ItemDescription>
+                            </ClickableItem>
+                    </Item>
+                    ))}                 
+                </Section>
+            </Container>
+        </Wrapper>
     );
 }
--- src/screens/Settings/styles.js
+++ src/screens/Settings/styles.js
@@ -1,5 +1,85 @@
 import styled from 'styled-components/native';
 
-export const Container = styled.View`
+export const Wrapper = styled.View`
+    flex: 1;
+    background-color: #000;
+`;
+
+export const Container = styled.ScrollView`
   
+`;
+
+export const Header = styled.View`
+    color: #fff;
+    height: 360px;
+    background: #333;
+    padding: 10px 20px;
+`;
+
+export const HeaderHelp = styled.Text`
+    color: #00ab4b;
+    flex: 1;
+    text-align: right;
+    font-weight: bold;
+    font-size: 20px;
+`;
+
+export const HeaderUserInfo = styled.View`
+    align-items: center;
+`;
+
+export const HeaderMyAccount = styled.Text`
+    color: #fff;
+    font-size: 14px;
+    padding-top: 10px;
+`;
+
+export const Username = styled.Text`
+    color: #fff;
+    font-size: 25px;
+`;
+
+export const Fullname = styled.Text`
+    color: #fff;
+    font-size: 18px;
+`;
+
+export const ViewProfile = styled.TouchableOpacity`
+    flex-direction: row;
+    align-items: center;
+`;
+
+export const ViewProfileText = styled.Text`
+    color: #00ab4b;
+    font-size: 16px;
+    margin: 10px 2px 10px;
+`;
+
+export const Img = styled.Image`
+    width: 140px;
+    height: 140px;
+    margin-bottom: 20px;
+`;
+
+export const Section = styled.View`
+
+`;
+
+export const Item = styled.View.attrs({
+    borderBottomColor: '#888',
+    borderBottomWidth: 0.5,
+})`
+    padding: 20px;
+`;
+
+export const ClickableItem = styled.TouchableOpacity``;
+
+export const ItemTitle = styled.Text`
+    color: #fff;
+    font-size: 16px;
+`;
+
+export const ItemDescription = styled.Text`
+    color: #999;
+    font-size: 14px;
 `;
